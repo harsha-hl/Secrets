0da87dcc65bb6b9466fdf8bee08531bb44d4856f
Harsha H L <harshahl@Harshas-MacBook-Air.local>
2022-05-11 22:59:20 +0530

    Add ENV vars

--- app.js
+++ app.js
@@ -17,8 +17,10 @@ const userSchema = new mongoose.Schema({
     password: String
 });
 
+console.log(process.env.API_KEY);
+
 const secret = "MyNameIsHarshaHL";
-userSchema.plugin(encrypt,{secret: secret, encryptedFields: ["password"]});
+userSchema.plugin(encrypt,{secret: process.env.SECRET, encryptedFields: ["password"]});
 
 const User = new mongoose.model('User', userSchema);
 
