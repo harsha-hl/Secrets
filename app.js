679bef21478972bfffc63757a0eda4425b4b8dbc
Harsha H L <harshahl@Harshas-MacBook-Air.local>
2022-05-12 17:35:33 +0530

    Add Level 3 Authentication - Hashing

--- app.js
+++ app.js
@@ -3,7 +3,8 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const ejs  = require('ejs');
 const mongoose = require('mongoose');
-const encrypt = require('mongoose-encryption');
+// const encrypt = require('mongoose-encryption'); // Used in level 2
+const md5 = require('md5');
 
 const app = express();
 
@@ -19,8 +20,7 @@ const userSchema = new mongoose.Schema({
 });
 
 // console.log(process.env.API_KEY);
-
-userSchema.plugin(encrypt,{secret: process.env.SECRET, encryptedFields: ["password"]});
+// userSchema.plugin(encrypt,{secret: process.env.SECRET, encryptedFields: ["password"]});
 
 const User = new mongoose.model('User', userSchema);
 
@@ -38,7 +38,7 @@ app.get('/login', function(req, res){
 
 app.post('/login', function(req, res){
     const username = req.body.username;
-    const password = req.body.password;
+    const password = md5(req.body.password);
     User.findOne({username: username}, function(err, user){
         if(err)console.log(err);
         else if(user){
@@ -50,7 +50,7 @@ app.post('/login', function(req, res){
 
 app.post('/register', function(req, res){
     const username = req.body.username;
-    const password = req.body.password;
+    const password = md5(req.body.password);
     const user = new User({
         username: username,
         password: password
